a87bd3a12650ea323b3b686be2607d52ce35e69d
Arbaj Raza <mdrazatech@gmail.com>
2025-03-03 12:06:24 +0530

    Initial commit

---
+++ js/kf.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function openPopUp(img)
+{
+
+window.open ('popup.htm?i='+img, 'wind', 'width=640,height=480')
+//window.open ('popup.htm?i='+img, 'wind', 'toolbar,width=150,height=100')
+
+}
+
+}
+/*
+     FILE ARCHIVED ON 23:51:21 Jun 03, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:16:59 Mar 03, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.569
+  exclusion.robots: 0.023
+  exclusion.robots.policy: 0.01
+  esindex: 0.011
+  cdx.remote: 10.504
+  LoadShardBlock: 169.354 (3)
+  PetaboxLoader3.datanode: 164.369 (5)
+  PetaboxLoader3.resolve: 207.245 (3)
+  load_resource: 229.674 (2)
+*/
